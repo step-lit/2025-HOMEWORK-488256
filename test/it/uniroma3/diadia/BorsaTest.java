928a57550733a54f9ab5d2aa809c36ed15def190
insertsurnamehere <luc.massari64@gmail.com>
2025-05-14 20:16:45 +0200

    ‘Test_per_Borsa_che_ha_ordinamenti_negli_strumenti’

--- test/it/uniroma3/diadia/BorsaTest.java
+++ test/it/uniroma3/diadia/BorsaTest.java
@@ -2,11 +2,23 @@ package it.uniroma3.diadia;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 import it.uniroma3.diadia.giocatore.Borsa;
+import it.uniroma3.diadia.giocatore.OrdineBorsaPesoCrescenteONomeCrescente;
+import it.uniroma3.diadia.giocatore.OrdinePerNOme;
 
 class BorsaTest {
 
@@ -14,7 +26,7 @@ class BorsaTest {
 	private Attrezzo foglio;
 	private Attrezzo torcia;
 	private Attrezzo martello;
-	
+
 	@BeforeEach
 	public void setUp() {
 		this.borsaTest = new Borsa();
@@ -22,26 +34,18 @@ class BorsaTest {
 		this.torcia = new Attrezzo("Torcia", 1);
 		this.martello = new Attrezzo("Martello", 2);
 	}
-	
+
 	/*********************************************************************
 	 *    metodi di test per il metodo AddAttrezzo(Attrezzo attrezzo)    *
 	 *********************************************************************/
-	
+
 	@Test
 	public void testAddAttrezzo() {
 		//la borsa inizialmente non ha attrezzi
 		assertTrue(this.borsaTest.addAttrezzo(this.martello));
 	}
-	
-	@Test
-	public void testAddAttrezzoBorsaPiena() {
-		//la borsa è piena (10 attrezzi)
-		for(int i=0; i<10; i++) {
-			this.borsaTest.addAttrezzo(this.foglio); //aggiunge 10 fogli di peso 0
-		}
-		assertFalse(this.borsaTest.addAttrezzo(this.foglio)); //da false perché piena
-	}
-	
+
+
 	@Test
 	public void testAddAttrezzoMaxPeso() {
 		//la borsa ha raggiunto il peso max
@@ -50,19 +54,19 @@ class BorsaTest {
 		}
 		assertFalse(this.borsaTest.addAttrezzo(this.martello)); //da false perché supera il peso max
 	}
-	
-	
-	
+
+
+
 	/********************************************************************
 	 * metodi di test per il metodo removeAttrezzo(String nomeAttrezzo) *
 	 ********************************************************************/
-	
+
 	@Test
 	public void testRemoveAttrezzoNoAttrezzi() {
 		//la borsa inizialmente non ha attrezzi
 		assertNull(this.borsaTest.removeAttrezzo("Martello"));
 	}
-	
+
 	@Test
 	public void testRemoveAttrezzo() {
 		//la borsa contiene l'attrezzo martello
@@ -79,19 +83,19 @@ class BorsaTest {
 		this.borsaTest.addAttrezzo(this.foglio);
 		assertNull(borsaTest.removeAttrezzo("Martello"));
 	}
-	
-	
-	
+
+
+
 	/*********************************************************************
 	 *   metodi di test per il metodo getAttrezzo(String nomeAttrezzo)   *
 	 *********************************************************************/
-	
+
 	@Test
 	public void testGetAttrezzoNoAttrezzi() {
 		//la borsa inizialmente non ha attrezzi
 		assertNull(this.borsaTest.getAttrezzo("Foglio"));
 	}
-	
+
 	@Test
 	public void testGetAttrezzo() {
 		//la borsa ha l'attrezzo cercato
@@ -100,7 +104,7 @@ class BorsaTest {
 		this.borsaTest.addAttrezzo(this.martello);
 		assertEquals(this.foglio, this.borsaTest.getAttrezzo("Foglio"));
 	}
-	
+
 	@Test
 	public void testGetAttrezzoNonPresente() {
 		//la borsa non ha l'attrezzo cercato
@@ -109,5 +113,46 @@ class BorsaTest {
 		assertNull(this.borsaTest.getAttrezzo("Martello"));
 		System.out.println("è");
 	}
-	
+
+	@Test 
+	public void testGetContenutoOrdinatoPerNome() {
+		Attrezzo attrezzo2 = new Attrezzo("ps", 5);
+		this.borsaTest.addAttrezzo(attrezzo2);
+		Attrezzo attrezzo4 = new Attrezzo("libro", 5);
+		this.borsaTest.addAttrezzo(attrezzo4);
+		SortedSet<Attrezzo> a = new TreeSet<Attrezzo>(new OrdinePerNOme());
+		a.addAll(Set.of(attrezzo4, attrezzo2));
+		assertEquals(a, this.borsaTest.getContenutoOrdinatoPerNome());
+	}
+	@Test 
+	public void getContenutoOrdinatoPerPeso() {
+		Attrezzo attrezzo1 = new Attrezzo("ps", 4);
+		this.borsaTest.addAttrezzo(attrezzo1);
+		Attrezzo attrezzo2 = new Attrezzo("libro", 5);
+		this.borsaTest.addAttrezzo(attrezzo2);
+		List<Attrezzo> a = new ArrayList<Attrezzo>(List.of( attrezzo1, attrezzo2));
+		assertEquals(a, this.borsaTest.getContenutoOrdinatoPerPeso());
+	}
+	@Test
+	public void testGetContenutoRaggruppatoPerPeso() {
+		Attrezzo piuma1 = new Attrezzo("piuma1", 1);
+		this.borsaTest.addAttrezzo(piuma1);
+		Attrezzo piuma2 = new Attrezzo("piuma2", 1);
+		this.borsaTest.addAttrezzo(piuma2);
+		//assertEquals(HashMap<Integer,Set<Attrezzo>>.ofEntries(entry(1, Set.of(new Attrezzo("piuma2", 1), (new Attrezzo("piuma2", 1))))), null);
+		Map<Integer,Set<Attrezzo>> a = new TreeMap<Integer,Set<Attrezzo>>();
+		a.put(Integer.valueOf(1),Set.of(piuma1, piuma2));
+		assertEquals(a, this.borsaTest.getContenutoRaggruppatoPerPeso());
+	}
+	@Test 
+	public void testGetSortedSetOrdinatoPerPeso() {
+		SortedSet<Attrezzo> a = new TreeSet<Attrezzo>(new OrdineBorsaPesoCrescenteONomeCrescente());
+		Attrezzo attrezzo1 = new Attrezzo("ps", 4);
+		this.borsaTest.addAttrezzo(attrezzo1);
+		a.add(attrezzo1);
+		Attrezzo attrezzo2 = new Attrezzo("libro", 5);
+		this.borsaTest.addAttrezzo(attrezzo2);
+		a.add(attrezzo2);
+		assertEquals(a, this.borsaTest.getSortedSetOrdinatoPerPeso());
+	}
 }
