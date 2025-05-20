b921aec9fac57eaeb7089f5b545761771fec73fe
insertsurnamehere <luc.massari64@gmail.com>
2025-05-20 14:46:02 +0200

    errore con fabbricaComandi

--- src/it/uniroma3/diadia/comandi/FabbricaDiComandi.java
+++ src/it/uniroma3/diadia/comandi/FabbricaDiComandi.java
@@ -2,6 +2,6 @@ package it.uniroma3.diadia.comandi;
 
 public interface FabbricaDiComandi {
 	
-	public Comando costruisciComando(String istruzione);
+	public Comando costruisciComando(String istruzione) throws Throwable;
 	
 }
